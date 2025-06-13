91905dd3d559f1bfa1cb75b96d521eeaeefbcc51
hugoMGSung <personar95@naver.com>
2025-06-13 16:01:14 +0900

    15일 완전커밋

--- day10/Day10Study/MyPortfolioWebApp/Controllers/NewsController.cs
+++ day10/Day10Study/MyPortfolioWebApp/Controllers/NewsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyPortfolioWebApp.Models;
+using System.Diagnostics;
 
 namespace MyPortfolioWebApp.Controllers
 {
@@ -108,10 +109,43 @@ namespace MyPortfolioWebApp.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         // <form asp-controller="News" asp-action="Create"> 이 http://localhost:5234/News/Create 포스트메서드 호출
-        public async Task<IActionResult> Create([Bind("Id,Title,Description")] News news)
-        { 
+        // News 모델에 저장하는 것은 파일경로
+        // IFormFile? UploadFile에는 파일자체 바이너리 데이터
+        public async Task<IActionResult> Create([Bind("Id,Title,Description")] News news, IFormFile? UploadFile)
+        {
+            const long MaxFileSize = 10 * 1024 * 1024;
+
             if (ModelState.IsValid)
             {
+                // 파일이 존재하면
+                if (UploadFile != null && UploadFile.Length > 0) { 
+                    // 서버에 파일저장, 모델에 파일경로 저장
+                    Debug.WriteLine(UploadFile.Length);
+
+                    if (UploadFile.Length > MaxFileSize)
+                    {
+                        ModelState.AddModelError("UploadFile", "파일크기는 10MB 이하로 제한합니다.");
+                        return View(news);
+                    }
+
+                    string upFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "upload");
+                    Directory.CreateDirectory(upFolder); // 폴더가 없으면 생성
+
+                    // example.jpg파일이 여러번올라가면 파일이 겹쳐짐
+                    // 파일명을 변경
+                    // Guid.NewGuid() = 랜덤아이디 생성
+                    // Path.GetExetension() = 파일의 확장자만 가져옴 .jpg
+                    string newFileName = Guid.NewGuid() + Path.GetExtension(UploadFile.FileName); // bcc5206a-1718-4885-929c-8c9060ba9374.jpg
+                    string filePath = Path.Combine(upFolder, newFileName); 
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await UploadFile.CopyToAsync(stream); // 파일저장
+                    }
+
+                    // 모델에 파일명 할당
+                    news.UploadFile = newFileName;
+                }
                 news.Writer = "관리자"; // 작성자는 자동으로 관리자
                 news.PostDate = DateTime.Now; // 게시일자는 현재
                 news.ReadCount = 0; 
@@ -149,7 +183,7 @@ namespace MyPortfolioWebApp.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description")] News news)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description")] News news, IFormFile? NewFile)
         {
             if (id != news.Id)
             {
@@ -169,6 +203,28 @@ namespace MyPortfolioWebApp.Controllers
 
                     existingNews.Title = news.Title;
                     existingNews.Description = news.Description;
+
+                    // 파일이 변경되었으면
+                    if (NewFile != null && NewFile.Length > 0)
+                    {
+                        string upFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "upload");
+                        Directory.CreateDirectory(upFolder); // 폴더가 없으면 생성
+
+                        // example.jpg파일이 여러번올라가면 파일이 겹쳐짐
+                        // 파일명을 변경
+                        // Guid.NewGuid() = 랜덤아이디 생성
+                        // Path.GetExetension() = 파일의 확장자만 가져옴 .jpg
+                        string newFileName = Guid.NewGuid() + Path.GetExtension(NewFile.FileName); // bcc5206a-1718-4885-929c-8c9060ba9374.jpg
+                        string filePath = Path.Combine(upFolder, newFileName);
+
+                        using (var stream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await NewFile.CopyToAsync(stream); // 파일저장
+                        }
+
+                        // 이전뉴스 파일명 할당
+                        existingNews.UploadFile = newFileName;
+                    }
 
                     // UPDATE News SET ...
                     //_context.Update(news); // 방식1 ID가 같은 새글을 UPDATE하면 수정                    
